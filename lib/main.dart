5dc7dbbcd67560e9c6807402877ba9da3295cb19
Mohsin Raza <71509915+mohsinraza079@users.noreply.github.com>
2021-01-08 23:48:31 +0500

    SplashScreen Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notes_taking_app/screens/note_list_screen.dart';
 import 'package:flutter_notes_taking_app/screens/note_view_screen.dart';
+import 'package:flutter_notes_taking_app/splaashscreen.dart';
 
 import 'package:provider/provider.dart';
 
@@ -8,7 +9,20 @@ import 'db_helper/note_provider.dart';
 import 'screens/note_edit_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(SplashScreen());
+}
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
---
+++ lib/splaashscreen.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_notes_taking_app/main.dart';
+
+
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 5),
+            () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => MaterialApp(
+            title: 'Notes App',
+            home: MyApp(),
+            theme: ThemeData(
+                primarySwatch: Colors.green)))));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image(image: AssetImage('images/abc.jpg'), height: 300),
+          Text("Mohsin Raza", style: TextStyle(fontSize: 20,color: Color(0xFFFD5872))),
+          SizedBox(
+            height: 10,
+          ),
+          Text("FA17-BSE-079", style: TextStyle(fontSize: 20,color: Color(0xFFFD5872))),
+          SizedBox(
+            height: 10,
+          ),
+          CircularProgressIndicator()
+        ],
+      ),
+    );
+  }
+}
